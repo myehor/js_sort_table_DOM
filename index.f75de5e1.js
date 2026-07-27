872ca6d21939ec3552b2b3630fad9d44703f7efc
Yehor Mishchenko <130080322+myehor@users.noreply.github.com>
2026-07-27 20:58:00 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+// write code here
+const table = document.querySelector("table");
+const tBody = document.querySelector("tbody");
+const rowsFromTBody = [
+    ...tBody.querySelectorAll("tr")
+];
+table.addEventListener("click", (e)=>{
+    if (e.target.tagName !== "TH") return;
+    const columnIndex = e.target.cellIndex;
+    rowsFromTBody.sort((firstRow, secondRow)=>{
+        if (columnIndex === 0 || columnIndex === 1) return firstRow.children[columnIndex].textContent.localeCompare(secondRow.children[columnIndex].textContent);
+        if (columnIndex === 3) return getSalaryNumber(firstRow.children[columnIndex].textContent) - getSalaryNumber(secondRow.children[columnIndex].textContent);
+        return Number(firstRow.children[columnIndex].textContent) - Number(secondRow.children[columnIndex].textContent);
+    });
+    for (const elem of rowsFromTBody)tBody.append(elem);
+});
+const getSalaryNumber = function(str) {
+    return Number(str.replaceAll(",", "").replace("$", ""));
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
